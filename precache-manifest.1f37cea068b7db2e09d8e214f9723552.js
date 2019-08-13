9bfaebdd3e6b02a1de56a515c1617e1ff4b2c4ce
v1sen2on <v1sen2on@gmail.com>
2019-08-13 01:35:41 +0000

    Updates

---
+++ precache-manifest.1f37cea068b7db2e09d8e214f9723552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360f72ad5ac7d7459c17d262881e8d3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17bfef673f6bc19a2a2a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a5f0a638b9bad8f408f8",
+    "url": "/static/js/2.7a8fd0b2.chunk.js"
+  },
+  {
+    "revision": "17bfef673f6bc19a2a2a",
+    "url": "/static/js/main.8bea87ed.chunk.js"
+  },
+  {
+    "revision": "a765e8d02f4000b4c802",
+    "url": "/static/js/runtime~main.31c8ad95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
